ea11d814e57946fe9603711c5d54d6f776b3f934
3deep0019 <trideepghosh83@gmail.com>
2020-09-23 23:24:26 +0530

    FSJ0820B106

--- src/dao/PlayerDAO.java
+++ src/dao/PlayerDAO.java
@@ -1,1 +1,49 @@
+  
+package dao;
 
+import java.io.IOException;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import model.Player;
+import model.Skill;
+import utility.ConnectionManager;
+
+public class PlayerDAO {
+	
+	public List<Player> getAllPlayer() throws ClassNotFoundException, SQLException, IOException{
+		
+		List<Player> playerList = new ArrayList<Player>();
+		
+		
+		
+		ConnectionManager con =new ConnectionManager();
+		
+		Statement st= con.getConnection().createStatement();
+		
+		String sql=" SELECT * FROM PLAYER ORDER BY NAME";
+		ResultSet resultSet = st.executeQuery(sql);
+		
+		Skill skill = null;
+		SkillDAO skilldao = new SkillDAO();
+		
+		while(resultSet.next())
+		{
+		
+			long playerId = resultSet.getLong(1);
+			String playerName = resultSet.getString(2);
+			String playerCountry = resultSet.getString(3);
+			skill = skilldao.getSkillID(resultSet.getLong(4));
+			Player player = new Player(playerId, playerName, playerCountry, skill);
+			playerList.add(player);
+
+		}
+		
+		return playerList;
+
+	}
+	
+}
--- src/dao/SkillDAO.java
+++ src/dao/SkillDAO.java
@@ -1,1 +1,43 @@
+package dao;
 
+import java.io.IOException;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import model.Player;
+import model.Skill;
+import utility.ConnectionManager;
+
+public class SkillDAO{
+	
+	public Skill getSkillID(Long ID) throws ClassNotFoundException, SQLException, IOException {
+		
+	
+		
+		ConnectionManager con =new ConnectionManager();
+		
+		Statement st= con.getConnection().createStatement();
+		
+		String sql=" SELECT * FROM SKILL WHERE ID =" +ID;;
+		ResultSet rs = st.executeQuery(sql);
+		
+		Skill skill = null;
+		
+		while(rs.next())
+		{
+
+			if(rs.getLong("ID") == ID)
+			{
+				skill = new Skill(rs.getLong("ID"), rs.getString("NAME"));
+			}
+			
+			
+		}
+		
+		return skill;
+	}
+
+}
